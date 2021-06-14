a903571b53b5df960c97002143724451f4801bbf
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-06-14 11:28:58 +0530

    [Mayur] Add : Welcome message added...

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Linked List Program!");
+        }
+    }
+}
